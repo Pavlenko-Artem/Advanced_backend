4b24a0e5aa27ee0c154771637c4b8320189ec2aa
Artem Pavlenko <artempavlenko1703@gmail.com>
2023-05-10 02:44:35 +0300

    Use Swagger in RolesController

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,16 +1,23 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { CreateRoleDto } from './dto/create-role.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Role } from './roles.model';
 
+@ApiTags('Роли')
 @Controller('roles')
 export class RolesController {
   constructor(private roleServise: RolesService) {}
 
+  @ApiOperation({ summary: 'Создание роли пользователя' })
+  @ApiResponse({ status: 200, type: Role })
   @Post()
   create(@Body() roleDto: CreateRoleDto) {
     return this.roleServise.createRole(roleDto);
   }
 
+  @ApiOperation({ summary: 'Получение роли по значению' })
+  @ApiResponse({ status: 200, type: Role })
   @Get('/:value')
   getByValue(@Param('value') value: string) {
     return this.roleServise.getRoleByValue(value);
