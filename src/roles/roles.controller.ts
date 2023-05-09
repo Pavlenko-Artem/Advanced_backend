c06d5d9dfaf0214f9810df9ab941b2e1f8dc09c7
Artem Pavlenko <artempavlenko1703@gmail.com>
2023-05-10 02:41:07 +0300

    Create create and getByValue in RolesController

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { RolesService } from './roles.service';
+import { CreateRoleDto } from './dto/create-role.dto';
 
 @Controller('roles')
-export class RolesController {}
+export class RolesController {
+  constructor(private roleServise: RolesService) {}
+
+  @Post()
+  create(@Body() roleDto: CreateRoleDto) {
+    return this.roleServise.createRole(roleDto);
+  }
+
+  @Get('/:value')
+  getByValue(@Param('value') value: string) {
+    return this.roleServise.getRoleByValue(value);
+  }
+}
