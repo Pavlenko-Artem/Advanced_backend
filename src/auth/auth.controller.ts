e1a40fcfba2ad3edb3092404c13754776c36f06e
Artem Pavlenko <artempavlenko1703@gmail.com>
2023-05-10 23:05:58 +0300

    Implement user registration and authentication

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,24 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { AuthService } from './auth.service';
+
+@ApiTags('Роли')
+@Controller('auth')
+export class AuthController {
+  constructor(private authServise: AuthService) {}
+
+  @ApiOperation({ summary: 'Аутентификация пользователя' })
+  @ApiResponse({ status: 200 })
+  @Post('/login')
+  login(@Body() userDto: CreateUserDto) {
+    return this.authServise.login(userDto);
+  }
+
+  @ApiOperation({ summary: 'Регистрация пользователя' })
+  @ApiResponse({ status: 200 })
+  @Post('/registration')
+  registration(@Body() userDto: CreateUserDto) {
+    return this.authServise.registration(userDto);
+  }
+}
